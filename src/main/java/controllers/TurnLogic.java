783af1f97e54376354d10a361ed9c93ab59c9c72
Mik1812 <s183913@student.dtu.dk>
2020-01-09 13:45:40 +0100

    Metodeopdelt roll dice i turnlogic

--- src/main/java/controllers/TurnLogic.java
+++ src/main/java/controllers/TurnLogic.java
@@ -13,7 +13,7 @@ public class TurnLogic {
     protected TxtReader cardsTxt;
     ChanceDeck chanceDeck;
     protected final Die die = new Die();
-    protected final Die die2 = new Die();
+    int roll1, roll2, rollSum;
 
     public void init(Board board, GUILogic guiLogic, TxtReader turnLogicTxt, TxtReader cardsTxt){
         this.board = board;
@@ -46,24 +46,20 @@ public class TurnLogic {
         
                 hasThrown = true;
     
-                //Roll the die
-                die.roll();
-                int roll1 = die.getFaceValue();
-                die2.roll();
-                int roll2 = die2.getFaceValue();
-                int roll = roll1+roll2;
-                player.setLastRoll((roll));
+                //Roll the dice
+                rollDice();
+                player.setLastRoll((rollSum));
                 guiLogic.displayDie(roll1, roll2);
-    
+                
                 //Calculate and move to next location
-                Square nextLocation = board.nextLocation(player, roll);
+                Square nextLocation = board.nextLocation(player, rollSum);
                 player.setLocation(nextLocation);
                 guiLogic.movePiece(player, player.getLastRoll());
     
                 //Apply the square's effect to the player
                 String message = nextLocation.landedOn(player);
     
-                //checker om en spiller har købt en grund. Hvis den har, så opdaterer GUILogic til at vise den nye ejer af grunden.
+                //checker om en spiller har købt en grund. Hvis vedkommende har, så opdaterer GUILogic til at vise den nye ejer af grunden.
                 if (message.charAt(message.length()-1) == 'T'){
                     guiLogic.setSquareOwner(player);
                     message = message.substring(0,message.length()-1);
@@ -129,8 +125,14 @@ public class TurnLogic {
         }
     }
     
-
-
+    private void rollDice(){
+        die.roll();
+        roll1 = die.getFaceValue();
+        die.roll();
+        roll2 = die.getFaceValue();
+        rollSum = roll1+roll2;
+    }
+    
     private void updateGUI(Player player){
 
     }
