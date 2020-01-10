2d429f42e36b14809eb45014e1d85d4251f3bd49
Volkan Isik <s180103@student.dtu.dk>
2020-01-10 13:51:14 +0100

    landedon metoden testet

--- src/test/java/domain/squares/SquareTest.java
+++ src/test/java/domain/squares/SquareTest.java
@@ -1,5 +1,6 @@
 package domain.squares;
 
+import domain.Piece;
 import domain.Player;
 import org.junit.Test;
 
@@ -8,20 +9,21 @@ import static org.junit.Assert.*;
 public class SquareTest {
     Square square0 = new TaxSquare("square0",0,null);
     Square square1 = new PropertySquare("square1",1,null,1000,500,"PropertySquare","red");
-    Square square2 = new GoToJailSquare("square2",2,null,null);
-    Square square3 = new RegularSquare("square3",3,null);
+    Square square2 = new RegularSquare("square2",2,null);
+    Square square3 = new GoToJailSquare("square3",3,null,square2);
     Square square4 = new ChanceSquare("square4",4,null,null);
-    Player testPlayer = new Player("Test",1000,null);
+    Piece testpiece = new Piece(square0);
+    Player testPlayer = new Player("Test",1000,testpiece);
+
     @Test
     public void landedOn() {
         assertEquals("Tax square",square0.landedOn(testPlayer));
 
         assertEquals("Not owned "+"PropertySquare"+" squareT",square1.landedOn(testPlayer));
 
-//        assertEquals("GoToJail square",square2.landedOn(testPlayer));
-//        assertEquals(true,testPlayer.getJail());
+        assertEquals("Regular square",square2.landedOn(testPlayer));
 
-        assertEquals("Regular square",square3.landedOn(testPlayer));
+        assertEquals("GoToJail square",square3.landedOn(testPlayer));
         assertEquals("Press OKS",square4.landedOn(testPlayer));
     }
 }
