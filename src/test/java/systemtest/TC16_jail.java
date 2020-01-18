30a9a2c16145eca311d9af595ffa7a21cc29cde6
Mik1812 <s183913@student.dtu.dk>
2020-01-18 13:06:14 +0100

    Begyndt test af sidste chancekort

--- src/test/java/systemtest/TC16_jail.java
+++ src/test/java/systemtest/TC16_jail.java
@@ -9,43 +9,21 @@ public class TC16_jail {
         //Tester de forskellige måder at komme ind og ud af jail
         new GameDriver(new int[]{
                 
-                3,3,3,3,3,3,
-                1,-1,1,-1,
-                10, 10,
-                1,2,
-                1,-1,1,-1,
-                1,2,
-                1,-1,1,-1,
-                1,2,
-                9,11,
-                1,-1,1,-1,
-                1,1,
-                10,10,
-                1,2,
-                1,-1,1,-1,
-                1,2,
-                1,-1,1,-1,
-                1,2,
+                3,3,3,3,3,3,10,10,1,2,1,2,1,2,9,11,1,1,10,10,1,2,1,2,1,2,
         
         }, new int[]{7500, 30000, 30000}, new int[]{0, 0, 0}, new int[]{1,2,3,4,5,6}).playGame();
     }
     
 //            3,3,3,3,3,3, Man kommer i fængsel ved at slå 3 ens
-//            1,-1,1,-1, De andre spillere springes over
 //            10, 10, Spilleren køber sig ud slår to ens og kommer i fængsel og får derfor sit første kast med det samme
 //            1,2, Spilleren slår med terningerne men kommer ikke ud
-//            1,-1,1,-1, De andre spillere springes over
 //            1,2, Spilleren slår med terningerne men kommer ikke ud
-//            1,-1,1,-1, De andre spillere springes over
 //            1,2 Spilleren slår med terningerne men kommer ikke ud og skal derfor betale for at komme ud og må herefter tage sin tur
 //            9,11, Spilleren kommer i fængsel uden at slå to ens, og får derfor ikke et sit første kast med det samme
-//            1,-1,1,-1, De andre spillere springes over
 //            1,1, Spilleren vælger at slå terningerne og kommer ud og må tage sin tur
 //            10,10, Spilleren slår to ens og kommer i fængsel og får derfor sit første kast med det samme
 //            1,2,Spilleren slår med terningerne men kommer ikke ud
-//            1,-1,1,-1,De andre spillere springes over
 //            1,2, Spilleren slår med terningerne men kommer ikke ud
-//            1,-1,1,-1, De andre spillere springes over
 //            1,2, Spilleren slår med terningerne men kommer ikke ud, da spilleren ikke har råd til at betale, har han tabt
 
 }
--- src/test/java/systemtest/TC18_chanceCards.java
+++ src/test/java/systemtest/TC18_chanceCards.java
@@ -1,4 +1,24 @@
 package systemtest;
 
+import controllers.GameDriver;
+
 public class TC18_chanceCards {
+    
+    public static void main(String[] args) {
+        
+        new GameDriver(new int[]{
+        
+                1,1,1,0,
+                1,1,
+                1,1,
+                4,3,
+                
+        }, new int[]{30000,30000,30000}, new int[]{0,0,0}, new int[]{
+                
+                18,18,18,18,
+                
+        }).playGame();
+        
+    }
+    
 }
