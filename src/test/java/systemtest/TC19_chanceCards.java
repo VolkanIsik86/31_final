49d44ddca4637c7359bf6101f0d85a642f25c3f5
Volkan Isik <s180103@student.dtu.dk>
2020-01-18 14:30:33 +0100

    test klasser gennavngivet

--- src/test/java/systemtest/TC19_chanceCards.java
+++ src/test/java/systemtest/TC19_chanceCards.java
@@ -1,4 +1,21 @@
 package systemtest;
 
+import controllers.GameDriver;
+
 public class TC19_chanceCards {
+
+    public static void main(String[] args) {
+
+        new GameDriver(new int[]{
+
+                1,1,1,0,1,1,1,1,4,3,16,17,15,15,1,1
+
+        }, new int[]{30000,30000,30000}, new int[]{0,0,0}, new int[]{
+
+                18,18,18,18,18,18,17
+
+        }).playGame();
+
+    }
+
 }
