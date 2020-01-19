02315544fac11b810171982605912c35f0c7386e
Andreas <S164933@student.dtu.dk>
2020-01-19 16:00:18 +0100

    Refractoring af TC navne

--- src/test/java/systemtest/TC16_StartTest.java
+++ src/test/java/systemtest/TC16_StartTest.java
@@ -2,7 +2,7 @@ package systemtest;
 
 import controllers.GameDriver;
 
-public class StartTest {
+public class TC16_StartTest {
 
     public static void main(String[] args) {
         new GameDriver(new int[]{3,4}, new int[]{30000, 30000, 30000}, new int[]{36,0,0}, new int[]{1,1,1,1,1,1,1,1}).playGame();
